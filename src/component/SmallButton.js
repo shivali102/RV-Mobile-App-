2b4e9fc3c8b279d36e6dae507f706b21d0b6f497
Pardeep Kohli <Pardeepkohli12@gmail.com>
2022-11-25 19:18:09 +0530

    Code Correction

--- src/component/SmallButton.js
+++ src/component/SmallButton.js
@@ -1,34 +1,35 @@
-import { View, Text, StyleSheet,TouchableOpacity } from 'react-native'
-import React from 'react'
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
-import color from '../theme/color'
-export default function SmallButton({ButtonName}) {
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import color from "../theme/color";
+export default function SmallButton({ ButtonName }) {
   return (
-    <View>
-       <View style={styles.ButtonoOuterView}>
-     <View style={styles.ButtonView}>
-        <TouchableOpacity>
-            <Text style={styles.ButtonText}>{ButtonName}</Text>
-        </TouchableOpacity>
-     </View>
-     </View>
-    </View>
-  )
+    <TouchableOpacity>
+      <View style={styles.ButtonoOuterView}>
+        <View style={styles.ButtonView}>
+          <Text style={styles.ButtonText}>{ButtonName}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
-const styles=StyleSheet.create({
-    ButtonText:{
-        textAlign:'center',
-        color:color.white,
-        fontSize:9,
-     fontWeight:'600',
-    },
-    ButtonView:{
-        borderRadius:4,       
-        backgroundColor:color.orange,
-        width:wp(33),
-        height:hp(4),
-        padding:6,
-        
-
-    },
-})
+const styles = StyleSheet.create({
+  ButtonText: {
+    textAlign: "center",
+    color: color.white,
+    fontSize: 9,
+    fontWeight: "600",
+  },
+  ButtonView: {
+    borderRadius: 4,
+    backgroundColor: color.orange,
+    width: wp(33),
+    height: hp(4),
+    padding: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
